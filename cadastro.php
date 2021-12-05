4ba98fe7bba48df016c8c9fea2de809a667bf1b8
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-12-05 11:02:05 -0300

    Adiciona a página de cadastro

---
+++ cadastro.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        type="text/css">
+    <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+</head>
+
+<body>
+    <div class="py-5 bg-warning">
+        <div class="container">
+            <div class="row">
+            </div>
+            <div class="row">
+                <div class="col-md-4 p-3"> <img class="img-fluid d-block" src="images/logo_palmares.png"> </div>
+                <div class="col-md-4 p-3"> <img class="img-fluid d-block" src="images/logo_saude.png"> </div>
+                <div class="col-md-4 p-3"> <img class="img-fluid d-block mx-auto mt-5" src="images/logo_aemasul.png"
+                        height="auto"> </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-5 text-center">
+        <div class="container">
+            <div class="row">
+                <div class="mx-auto col-lg-12 col-12">
+                    <h1>Inscrição do Processo Seletivo</h1>
+                    <p class="mb-3">Preencha os dados corretamente</p>
+                    <form class="text-left">
+                        <div class="form-group"> <label for="form16">Nome completo</label> <input type="text"
+                                class="form-control" id="form16" placeholder="Johann W. Goethe"> </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4"> <label for="form19">RG (Identidade)</label> <input
+                                    type="text" class="form-control" id="form19"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Orgão Expedidor</label> <input
+                                    type="text" class="form-control" id="form20"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Orgão UF</label> <input type="text"
+                                    class="form-control" id="form20"> </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4"> <label for="form19">CPF</label> <input type="text"
+                                    class="form-control" id="form19"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Data de nascimento</label> <input
+                                    type="text" class="form-control" id="form20"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Sexo</label>
+
+                                <select name="sexo" class="form-control">
+                                    <option value="M">Masculino</option>
+                                    <option value="F">Feminino</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="form16">CEP</label>
+                            <input type="text" class="form-control" id="form16" placeholder="Johann W. Goethe">
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4"> <label for="form19">Endereço</label> <input type="text"
+                                    class="form-control" id="form19"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Número</label> <input type="text"
+                                    class="form-control" id="form20"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Complemento</label>
+                                <input type="text" class="form-control" id="form20">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4"> <label for="form19">Bairro</label> <input type="text"
+                                    class="form-control" id="form19"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Cidade</label> <input type="text"
+                                    class="form-control" id="form20"> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Estado</label>
+                                <select name="estado" class="form-control">
+                                    <option value="Pernambuco">Pernambuco</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-7"> <label for="form19">E-mail</label> <input type="text"
+                                    class="form-control" id="form19"> </div>
+                            <div class="form-group col-md-5"> <label for="form20">Whatsapp</label> <input type="text"
+                                    class="form-control" id="form20"> </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4"> <label for="form19">Cargo</label> <select name="estado"
+                                    class="form-control">
+                                    <option value="Pernambuco">Pernambuco</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-4"> <label for="form20">Unidade de atendimento</label> <select
+                                    name="estado" class="form-control">
+                                    <option value="Pernambuco">Pernambuco</option>
+                                </select> </div>
+                            <div class="form-group col-md-4"> <label for="form20">Pessoa com deficiência</label> <br>
+                                <input class="form-check-input" type="checkbox" id="form21" value="on">
+                                <label class="form-check-label" for="form21"> Visual </label>
+
+                                <input class="form-check-input" type="checkbox" id="form21" value="on">
+                                <label class="form-check-label" for="form21"> Motora </label>
+
+                                <input class="form-check-input" type="checkbox" id="form21" value="on">
+                                <label class="form-check-label" for="form21"> Auditiva </label>
+
+                                <input class="form-check-input" type="checkbox" id="form21" value="on">
+                                <label class="form-check-label" for="form21"> Outra </label>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-6"> <label for="form19">Senha</label> <input type="password"
+                                    class="form-control" id="form19" placeholder="••••"> </div>
+                            <div class="form-group col-md-6"> <label for="form20">Confirmar senha</label> <input
+                                    type="password" class="form-control" id="form20" placeholder="••••"> </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-check"> <input class="form-check-input" type="checkbox" id="form21"
+                                    value="on"> <label class="form-check-label" for="form21"> Solicito isenção da taxa
+                                    de inscrição </label> </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-check"> <input class="form-check-input" type="checkbox" id="form21"
+                                    value="on"> <label class="form-check-label" for="form21"> Eu aceito os <a
+                                        href="#">Termos e Condições</a> </label> </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Inscrever-se</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
